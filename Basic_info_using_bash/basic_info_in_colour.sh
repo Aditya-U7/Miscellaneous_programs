74713b3c75567b5c758d20c3589b66b78083ba42
Aditya Upadhye <66748266+Aditya-U7@users.noreply.github.com>
2025-11-28 18:18:34 +0530

    Made the variable names of colours more consistent.

--- Basic_info_using_bash/basic_info_in_colour.sh
+++ Basic_info_using_bash/basic_info_in_colour.sh
@@ -7,25 +7,25 @@ This is a bash program to display basic information about your system. For netwo
 #!/bin/bash
 
 
-BOLDGREEN='\e[1;32m'
-RED_BOLD="\e[1;31m"
+BOLD_GREEN='\e[1;32m'
+BOLD_RED="\e[1;31m"
 DARK_YELLOW='\e[1;33m'
 MAGENTA='\e[1;35m'
 CYAN='\e[1;36m'
-ENDCOLOUR='\e[0m'
+END_COLOUR='\e[0m'
 
 
 os_info()
 {
 
 	os_name="$(uname -o)"
-	echo -e "${CYAN}Operating system:${ENDCOLOUR} ${MAGENTA}${os_name}${ENDCOLOUR}"
+	echo -e "${CYAN}Operating system:${END_COLOUR} ${MAGENTA}${os_name}${END_COLOUR}"
 
 	kernel_name="$(uname -s)"
-	echo -e "${CYAN}Kernel name:${ENDCOLOUR} ${MAGENTA}${kernel_name}${ENDCOLOUR}"
+	echo -e "${CYAN}Kernel name:${END_COLOUR} ${MAGENTA}${kernel_name}${END_COLOUR}"
 
 	kernel_version="$(uname -r)"
-	echo -e "${CYAN}Kernel version:${ENDCOLOUR} ${MAGENTA}${kernel_version}${ENDCOLOUR}"
+	echo -e "${CYAN}Kernel version:${END_COLOUR} ${MAGENTA}${kernel_version}${END_COLOUR}"
 
 }
 
@@ -47,7 +47,7 @@ cpu_info()
 
 	for key in "${!cpu_info[@]}"
 	do
-		echo -e "${CYAN}$key${ENDCOLOUR}: ${MAGENTA}${cpu_info[$key]}${ENDCOLOUR}"
+		echo -e "${CYAN}$key${END_COLOUR}: ${MAGENTA}${cpu_info[$key]}${END_COLOUR}"
 	done 
 
 }
@@ -66,14 +66,14 @@ network_info()
 		then 
 			network_name="$(nmcli dev show | grep -m 1 "GENERAL.CONNECTION" | cut -d':' -f2)"
 			network_name="${network_name##*( )}"
-			echo -e "${CYAN}Connected Network Name${ENDCOLOUR}: ${DARK_YELLOW}$network_name${ENDCOLOUR}"
+			echo -e "${CYAN}Connected Network Name${END_COLOUR}: ${DARK_YELLOW}$network_name${END_COLOUR}"
 
 			network_status="$(nmcli networking connectivity)"
 			if [[ $network_status == "full" ]]
 			then
-				echo -e "${CYAN}Internet status of${ENDCOLOUR} ${DARK_YELLOW}$network_name${ENDCOLOUR}: ${BOLDGREEN}Active${ENDCOLOUR}"             
+				echo -e "${CYAN}Internet status of${END_COLOUR} ${DARK_YELLOW}$network_name${END_COLOUR}: ${BOLD_GREEN}Active${END_COLOUR}"             
 			else
-				echo -e "${CYAN}Internet status of${ENDCOLOUR} ${DARK_YELLOW}$network_name${ENDCOLOUR}: ${RED_BOLD}Not active${ENDCOLOUR}"
+				echo -e "${CYAN}Internet status of${END_COLOUR} ${DARK_YELLOW}$network_name${END_COLOUR}: ${BOLD_RED}Not active${END_COLOUR}"
 			fi
 
 			dns_server_ipv4="$(nmcli dev show | grep "IP4.DNS" | cut -d':' -f2)"
@@ -83,19 +83,19 @@ network_info()
 			dns_server_ipv6="${dns_server_ipv6##*( )}"
 			if [[ -n $dns_server_ipv4 ]]
 			then
-				echo -e "${CYAN}DNS IPv4 ${ENDCOLOUR}:${MAGENTA} $dns_server_ipv4 ${ENDCOLOUR}"
+				echo -e "${CYAN}DNS IPv4 ${END_COLOUR}:${MAGENTA} $dns_server_ipv4 ${END_COLOUR}"
 			fi
 			if [[ -n $dns_server_ipv6 ]]
 			then
-				echo -e "${CYAN}DNS IPv6 ${ENDCOLOUR}:${MAGENTA} $dns_server_ipv6 ${ENDCOLOUR}"
+				echo -e "${CYAN}DNS IPv6 ${END_COLOUR}:${MAGENTA} $dns_server_ipv6 ${END_COLOUR}"
 			fi
 
 		else 
-			echo -e "${DARK_YELLOW}Network info: not connected to a network.${ENDCOLOUR}"
+			echo -e "${DARK_YELLOW}Network info: not connected to a network.${END_COLOUR}"
 		fi
 
 	else
-		echo -e "${RED_BOLD}Not available to fetch network info.${ENDCOLOUR}"
+		echo -e "${BOLD_RED}Not available to fetch network info.${END_COLOUR}"
 
 	fi
 
@@ -108,9 +108,9 @@ user_info()
 	current_user_name="$(whoami)"
 	current_users="$(who | cut -d' ' -f1 | sort | uniq)" 
 	system_name="$(hostname)"
-	echo -e "${CYAN}Current user name${ENDCOLOUR}: ${MAGENTA}${current_user_name}${ENDCOLOUR}"
-	echo -e "${CYAN}Logged in user(s)${ENDCOLOUR}: ${MAGENTA}${current_users}${ENDCOLOUR}"
-	echo -e "${CYAN}System name${ENDCOLOUR}: ${MAGENTA}${system_name}${ENDCOLOUR}"
+	echo -e "${CYAN}Current user name${END_COLOUR}: ${MAGENTA}${current_user_name}${END_COLOUR}"
+	echo -e "${CYAN}Logged in user(s)${END_COLOUR}: ${MAGENTA}${current_users}${END_COLOUR}"
+	echo -e "${CYAN}System name${END_COLOUR}: ${MAGENTA}${system_name}${END_COLOUR}"
 
 }
 
